db65a28b9b99b8e41019fa48242c0c5b996c5eba
Alexandre Flament <alexflam2003@gmail.com>
2023-03-01 18:48:00 +0100

    SelectedFile data

--- fileviewer.py
+++ fileviewer.py
@@ -1,4 +1,4 @@
-import pygame,os
+import pygame,os, json
 pygame.init()
 
 def scale_mouse_pos(screen_size):
@@ -10,15 +10,23 @@ def scale_mouse_pos(screen_size):
 class FileViewer():
 
     def __init__(self,file):
+
         self.mousepos = [-1,-1]
         self.mouseaction = [False, False]
         self.clicked = False
+
         self.file = file
         self.files = os.listdir(file)
+        self.initfstfile = self.files[0] 
+
         self.font = pygame.font.Font("PixelFont.ttf", 15)
         self.fvimg = pygame.image.load("fileviewer.png")
-        self.selectedfile = None    
-        self.initfstfile = self.files[0]   
+
+        self.selectedfile = None
+        self.selectedfiledata = None
+        self.nblayers = "N/A"
+        self.nbblocks = "N/A"
+        self.initpos = "N/A"
 
 
     ###################################################   DRAW FV   ###################################################
@@ -47,8 +55,43 @@ class FileViewer():
         if 3 <= self.mousepos[0] <= 382 and 46 <= self.mousepos[1] <= 277 and self.mouseaction[0]:
             y = (self.mousepos[1]-46)//23
             self.selectedfile = self.files[int(y)]
+            with open(f"Saves/{self.selectedfile}.json", 'r') as f:
+                self.selectedfiledata = json.load(f)
+            
+            self.nblayers = self.nbroflayers
+            self.nbblocks = self.nbrofblcks
+            self.initpos = self.campos
+
         if self.mouseaction[1]:
             self.selectedfile = None
+            self.selectedfiledata = None
+            self.nblayers = "N/A"
+            self.nbblocks = "N/A"
+            self.initpos = "N/A"
+
+
+    ###################################################   SAVESDATA   ###################################################
+
+    @property
+    def nbrofblcks(self):
+        if self.selectedfiledata == None:
+            return "N/A"
+        ln = 0
+        for lyer in self.selectedfiledata["map"]:
+            ln += len(self.selectedfiledata["map"][lyer])
+        return str(ln)
+    
+    @property
+    def nbroflayers(self):
+        if self.selectedfiledata == None:
+            return "N/A"
+        return str(len(self.selectedfiledata["all_layers"]))
+    
+    @property
+    def campos(self):
+        if self.selectedfiledata == None:
+            return "N/A"
+        return str(self.selectedfiledata["camera_pos"][0]) + ", " + str(self.selectedfiledata["camera_pos"][1])
 
 
     ###################################################   HANDLERS   ###################################################
